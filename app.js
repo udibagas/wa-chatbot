6382cd06149e7d6c5a7fbbcd553a8de18d25b082
Bagas Udi Sahsangka <udibagas@gmail.com>
2025-05-02 19:37:48 +0700

    handle spa routing

--- app.js
+++ app.js
@@ -28,6 +28,10 @@ app.use("/media", express.static(path.join(__dirname, "media")));
 
 app.use(require("./routes/index"));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client-app/dist/index.html"));
+});
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
